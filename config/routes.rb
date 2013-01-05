f1931d0ae4e390b30bece1b1b53e5b41593ab9b8
alucardpj <alucardpj@gmail.com>
2013-01-05 09:30:16 +0000

    add grape support

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Mpweixintest::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  mount MyAPI => "/"
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
