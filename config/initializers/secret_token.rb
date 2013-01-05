917400c4855d4a748dedf12a495af1a0c3d58b1b
alucardpj <alucardpj@gmail.com>
2013-01-05 07:46:10 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mpweixintest::Application.config.secret_token = '75b8d40e0aa2028064a099884bbb6cc28eb946fec9310b61e23f4183b512b4c61a785a419f7fe61f6bf15d6f5543724da22a5ae9aedaf2f70bbbc1fb06b5ce9a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mpweixintest::Application.config.session_store :cookie_store, key: '_mpweixintest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mpweixintest::Application.config.session_store :active_record_store
