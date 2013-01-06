5c4e52fec11fbb85477e6a0d06f9d0da062f86fc
alucardpj <alucardpj@gmail.com>
2013-01-06 06:35:30 +0000

    修改my_api.rb输出格式及接入校验

--- lib/my_api.rb
+++ lib/my_api.rb
@@ -1,21 +1,33 @@
+require 'digest/sha1'
+
 class MyAPI < Grape::API
   version 'v1', :using => :path
   format :xml
 
+  helpers do
+    def checksignature(signature, timestamp, nonce)
+      array = [Setting.token, timestamp, nonce].sort
+      signature == Digest::SHA1.hexdigest(array.join)
+    end
+  end
+
   desc "validation"
   get do
-    params[:echostr]
+    if checksignature(params[:signature], params[:timestamp], params[:nonce])
+      params[:echostr]
+    end
   end
 
   desc "reply"
   post do
+    Rails.logger.info params
     builder = Nokogiri::XML::Builder.new do |x|
       x.xml() {
         x.ToUserName {
-          x.cdata params[:xml][:ToUserName]
+          x.cdata params[:xml][:FromUserName]
         }
         x.FromUserName {
-          x.cdata params[:xml][:FromUserName]
+          x.cdata params[:xml][:ToUserName]
         }
         x.CreateTime Time.now.to_i.to_s
         x.MsgType {
@@ -27,5 +39,10 @@ class MyAPI < Grape::API
         x.FuncFlag("0")
       }
     end
+    #Rails.logger.info builder.doc.root.to_xml
+    output = builder.to_xml(:save_with => Nokogiri::XML::Node::SaveOptions::AS_XML | Nokogiri::XML::Node::SaveOptions::NO_DECLARATION).strip    
+    #builder.doc.root.to_xml
+    Rails.logger.info output
+    output
   end
 end
