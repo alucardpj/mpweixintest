91320e99e8f57269f1718a7a3dd4cf97f06046a8
alucardpj <alucardpj@gmail.com>
2013-03-04 16:46:21 +0800

    echo.xml.erb -> textreply.xml.erb,增加location位置解析

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -7,9 +7,18 @@ class WeixinsController < ApplicationController
   end
 
   def create
-    if params[:xml][:MsgType] == "text"
-      render "echo", :formats => :xml
+    # if params[:xml][:MsgType] == "text"
+    #   render "echo", :formats => :xml
+    # end
+    case params[:xml][:MsgType]
+    when "text"
+      @reply = params[:xml][:Content]
+    when "location"
+      url = "http://api.map.baidu.com/geocoder?location=#{params[:xml][:Location_X]},#{params[:xml:][:Location_Y]}&output=json&key=9d303595cfbaa7f96ab0e7f56c1fd29f"
+      result = JSON::parse(Net::HTTP.get(URI(url)))
+      @reply = result["result"]["formatted_address"]
     end
+    render "textreply", :formats => :xml
   end
 
   private
