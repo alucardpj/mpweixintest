93968ff7e09c35536c9964a1d1992b9d0431e82a
Peng Jian <alucardpj@gmail.com>
2013-03-04 09:03:15 +0000

    修正:xml: => :xml

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -14,7 +14,7 @@ class WeixinsController < ApplicationController
     when "text"
       @reply = params[:xml][:Content]
     when "location"
-      url = "http://api.map.baidu.com/geocoder?location=#{params[:xml][:Location_X]},#{params[:xml:][:Location_Y]}&output=json&key=9d303595cfbaa7f96ab0e7f56c1fd29f"
+      url = "http://api.map.baidu.com/geocoder?location=#{params[:xml][:Location_X]},#{params[:xml][:Location_Y]}&output=json&key=9d303595cfbaa7f96ab0e7f56c1fd29f"
       result = JSON::parse(Net::HTTP.get(URI(url)))
       @reply = result["result"]["formatted_address"]
     end
