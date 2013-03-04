52a9bf643eb108fde5e45e4b9e53968b4708e858
alucardpj <alucardpj@gmail.com>
2013-03-04 16:14:12 +0800

    更新rails版本到3.2.12,增加controller方式的接口实现

---
+++ app/controllers/weixins_controller.rb
@@ -0,0 +1,20 @@
+class WeixinsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  before_filter :check_weixin_legality
+
+  def show
+    render :text => params[:echostr]
+  end
+
+  def create
+    if params[:xml][:MsgType] == "text"
+      render "echo", :formats => :xml
+    end
+  end
+
+  private
+  def check_weixin_legality
+    array = [Setting.token, params[:timestamp], params[:nonce]].sort
+    render :text => "Forbidden", :status => 403 if params[:signature] != Digest::SHA1.hexdigest(array.join)
+  end
+end
---
+++ app/helpers/weixins_helper.rb
@@ -0,0 +1,2 @@
+module WeixinsHelper
+end
