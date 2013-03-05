6710bdbd6124335fa0428ea1a87e78c4163f53dd
Peng Jian <alucardpj@gmail.com>
2013-03-05 07:06:03 +0000

    删除废弃代码

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -6,19 +6,6 @@ class WeixinsController < ApplicationController
     render :text => params[:echostr]
   end
 
-  # def create
-  #   case params[:xml][:MsgType]
-  #   when "text"
-  #     @reply = params[:xml][:Content]
-  #   when "location"
-  #     url = "http://api.map.baidu.com/geocoder?location=#{params[:xml][:Location_X]},#{params[:xml][:Location_Y]}&output=json&key=9d303595cfbaa7f96ab0e7f56c1fd29f"
-  #     result = JSON::parse(Net::HTTP.get(URI(url)))
-  #     @reply = result["result"]["formatted_address"]
-  #   when "image"
-  #   end
-  #   render "textreply", :formats => :xml
-  # end
-
   def reply_text
     @reply = params[:xml][:Content]
     render "reply", formats: :xml
